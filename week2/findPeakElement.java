cd92dcd85ead96b811ccdc5cd2975baef3fca719
chenlei <chenleizhaoapplication@gmail.com>
2017-05-26 17:01:31 -0400

    medium

---
+++ week2/findPeakElement.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findPeakElement(int[] A) {
+         int start = 0, end = A.length-1;
+        while(start+1 < end){
+            int mid = start + (end - start)/2;
+            if(A[mid] < A[mid+1]){
+                start = mid;
+            }else if(A[mid-1] > A[mid]){
+                end = mid;
+            }else{
+                start = mid;//分不出波峰波谷的时候，随便划界限，也可以是end = mid;
+            }
+        }
+        if(A[start] > A[end])
+            return start;
+        else
+            return end;
+    }
+}
--- week2/reverseBit.java
+++ week2/reverseBit.java
@@ -1,11 +1,14 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int reversed = 0;
-        for (int i = 0; i < 32; i++) {
-            reversed = (reversed << 1) | (n & 1);
-            n = (n >> 1);
-        }
-        return reversed;
+    	int result = 0;
+    for (int i = 0; i < 32; i++) {
+    	//n&1 就是在取 1， n&0就是在取0
+        result += n & 1;
+        n >>>= 1;   // CATCH: must do unsigned shift
+        if (i < 31) // CATCH: for last digit, don't shift!
+            result <<= 1;
     }
+    return result;
+  }
 }
